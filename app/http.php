f0f6bfd0448ed079b7d2b17a4168c9bd7092828b
Damodar Lohani <dlohani48@gmail.com>
2023-07-13 07:50:48 +0000

    callbacks now on http server

--- app/http.php
+++ app/http.php
@@ -8,11 +8,15 @@ use Utopia\Http\Adapter\Swoole\Server;
 
 $server = new Server('0.0.0.0', '80');
 
-$server->onWorkerStart(function($server, $workerId) {
-    Console::info('Worker ' . ++$workerId . ' started');
-});
 
 $http = new Http($server, 'Asia/Kathmandu');
+
+$http->onWorkerStart(function($server, $workerId) {
+    Console::info('Worker ' . ++$workerId . ' started');
+});
+$http->onStart(function($server) {
+    Console::info('Server started. We can do the initializations for first time starting the server here');
+});
 
 $http->loadFiles(__DIR__ . '/../public/');
 

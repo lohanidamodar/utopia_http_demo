07fcc6777a55e55535d8c4297b27a00103ed7ce1
Damodar Lohani <dlohani48@gmail.com>
2023-07-13 08:37:42 +0000

    update with start hooks

--- app/http.php
+++ app/http.php
@@ -11,13 +11,6 @@ $server = new Server('0.0.0.0', '80');
 
 $http = new Http($server, 'Asia/Kathmandu');
 
-$http->onWorkerStart(function($server, $workerId) {
-    Console::info('Worker ' . ++$workerId . ' started');
-});
-$http->onStart(function($server) {
-    Console::info('Server started. We can do the initializations for first time starting the server here');
-});
-
 $http->loadFiles(__DIR__ . '/../public/');
 
 $http->start();

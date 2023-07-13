c0170399ab7d1290491a787cc54ece40755cd5ad
Damodar Lohani <dlohani48@gmail.com>
2023-07-13 08:50:32 +0000

    simpler

--- app/http.php
+++ app/http.php
@@ -1,10 +1,32 @@
 <?php
 
 require_once __DIR__ . '/init.php';
+require_once __DIR__ . '/controllers/default.php';
 
 use Utopia\CLI\Console;
 use Utopia\Http\Http;
 use Utopia\Http\Adapter\Swoole\Server;
+
+Http::setResource('database', function(){
+    return 'MariaDB';
+});
+
+Http::onStart()
+    ->inject('database')
+    ->action(function(string $database){
+        Console::info('Server started with database: ' . $database);
+    });
+
+Http::onStart()
+    ->action(function() {
+        Console::info('We can execute multiple hooks when server start');
+    });
+
+Http::onWorkerStart()
+    ->inject('workerId')
+    ->action(function($workerId) {
+        Console::info('Worker ' . ++$workerId . ' started');
+    });
 
 $server = new Server('0.0.0.0', '80');
 
